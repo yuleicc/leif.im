8831b879c9d0c1059db2edfbd68ce6eeef92bc9f
Leif <yulei@top-list.com.cn>
2019-03-29 12:00:16 +0800

    添加robots.txt、sitemap.xml文件

--- sitemaps.py
+++ sitemaps.py
@@ -1,14 +1,15 @@
 from django.contrib.sitemaps import Sitemap
-
+from apps.blog.models import Blog
 from django.urls import reverse
 
 
-class IndexSitemap(Sitemap):
+class BlogSitemap(Sitemap):
     changefreq = "daily"
-    priority = 1.0
+    priority = 0.5
+    protocol = "https"
 
     def items(self):
-        return ['home']
+        return Blog.objects.filter(is_public=True).filter(status='p').order_by('-created_at')
 
-    def location(self, item):
-        return reverse(item)
+    def lastmod(self, item):
+        return item.updated_at
--- urls.py
+++ urls.py
@@ -13,15 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.sitemaps.views import sitemap
 from apps.blog import views
+from sitemaps import BlogSitemap
+from django.contrib import admin
 from apps.blog.views import IndexView
-from sitemaps import IndexSitemap
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 
 sitemaps = {
-    'index': IndexSitemap,
+    'blog': BlogSitemap,
 }
 
 handler404 = 'apps.blog.views.handle_page_not_found'
@@ -30,5 +31,9 @@ handler500 = 'apps.blog.views.server_error_view'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.blog.urls', 'blog')),
+    path('robots.txt', TemplateView.as_view(
+        template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
     path('api/v1/', include('apis.api_v1.blog.urls', 'api-v1'))
 ]
